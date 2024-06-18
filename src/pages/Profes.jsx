16de6667e9e68cf1a48ce88d5ffe0824e23fff0a
Diego Olmi <diegolmi@hotmail.com>
2024-06-18 08:54:20 -0300

    profes

--- src/pages/Profes.jsx
+++ src/pages/Profes.jsx
@@ -1,9 +1,13 @@
 import { Container, Row, Col, Card } from "react-bootstrap";
 import Carrousel from "../components/Carrousel";
-import Profe1 from "../assets/img/profe1.png";
+import Profe1 from "../assets/img/agustin.png";
 import Profe2 from "../assets/img/profe2.png";
 import Profe3 from "../assets/img/profe3.png";
 import Profe4 from "../assets/img/profe4.png";
+import Profe5 from "../assets/img/francisco.png";
+import Profe6 from "../assets/img/ignacio.png";
+import Profe7 from "../assets/img/matias.png";
+import Profe8 from "../assets/img/sergio.png";
 
 const Profes = () => {
   // cada profesor deberia tener un nombre, una especialidad y una foto
@@ -16,41 +20,61 @@ const Profes = () => {
     {
       nombre: "Profesor 2",
       especialidad: "Especialista en delantera y arqueros",
-      foto:  Profe2,
+      foto: Profe2,
     },
     {
       nombre: "Profesor 3",
       especialidad: "Especialista en defensa y mediocampo",
-      foto: Profe3 ,
+      foto: Profe3,
     },
     {
       nombre: "Profesor 4",
       especialidad: "Especialista en delantera y arqueros",
-      foto:  Profe4,
+      foto: Profe4,
     },
     {
-      nombre: "Profesor 5",
+      nombre: "Francisco",
       especialidad: "Especialista en defensa y mediocampo",
-      foto: Profe1,
+      foto: Profe5,
     },
     {
-      nombre: "Profesor 6",
+      nombre: "Ignacio",
       especialidad: "Especialista en delantera y arqueros",
-      foto:  Profe1 ,
+      foto: Profe6,
+    },
+    {
+      nombre: "Matias",
+      especialidad: "Especialista en defensa y mediocampo",
+      foto: Profe7,
+    },
+    {
+      nombre: "Sergio",
+      especialidad: "Especialista en delantera y arqueros",
+      foto: Profe8,
     },
   ];
 
   return (
     <Container fluid className="containerProfes">
       <Carrousel />
-      <Row className="mt-5">
-        <h1>Profes</h1>
-        <p>
-          Nuestros profesores son los mejores, con años de experiencia en el
-          club y en la docencia.
-        </p>
+      <Row className="mt-5 text-center">
+        <Col>
+          <h1>Profes</h1>
+          <p>
+            Nuestros profesores son los mejores, con años de experiencia en el
+            club y en la docencia.
+          </p>
+        </Col>
+      </Row>
+      <Row className="mt-3 d-flex justify-content-center">
         {profesores.map((profesor, index) => (
-          <Col key={index} md={4}>
+          <Col
+            key={index}
+            xs={12}
+            sm={6}
+            md={4}
+            className="d-flex justify-content-center mb-4"
+          >
             <Card style={{ width: "18rem" }}>
               <Card.Img variant="top" src={profesor.foto} />
               <Card.Body>

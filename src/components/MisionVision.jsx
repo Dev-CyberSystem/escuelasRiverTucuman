1484cf84769d95c493e2cae6c0b2613bd9a04ce9
Diego Olmi <diegolmi@hotmail.com>
2024-06-18 09:35:05 -0300

    filosofia y objetivos

--- src/components/MisionVision.jsx
+++ src/components/MisionVision.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Container, Col, Row } from "react-bootstrap";
 import Player1 from "../assets/img/player1.png";
 import "./styleMisionVision.css";
+import escudo from "../assets/img/escudoRiver.png";
 
 const MisionVision = () => {
   return (
@@ -10,20 +12,29 @@ const MisionVision = () => {
           <img src={Player1} alt="Jugador" className="img-player1" />
         </Col>
         <Col>
-          <h1>Misión y Visión</h1>
-          <h2>Misión</h2>
-          <p>
-            La Escuela de Fútbol de River Plate tiene como misión formar
-            personas a través del fútbol, transmitiendo valores y conocimientos
-            que les permitan a los alumnos desarrollarse en el deporte y en la
-            vida.
+          <h1>FILOSOFIA RIVER</h1>
+           <p>
+            Más alla de sostener <b>"El Mas Grande"</b> , River quiere definirse por sus valores y sus valores tienen que ver con <b>"Vivir y Jugar con Grandeza"</b>, que abarca más que ser <b>"El Mas Grande"</b>. Porque la grandeza no esta al final del camino. Las formas, as convicciones, la manera de llegar a lo que te propones. <br />
+            Grandeza es un camino de humildad que esta en la historia de <b> RIVER </b>, en sus logros y en el aspiracional hacia un futuro. <br />
+            Grandeza para honrar nuestra filosofia de juego en cualquier campo, ser humildes en las victorias y sobreponernos a las derrotas, reconocer los errores y aprender de ellos, honrar a nuestro idolos y formar a nuestros jovenes, promover valores dentro y fuera de la cancha, ver en los rivales adversarios y no enemigos, anteponer las formas a los resultados, priorizar lo colectivo por sobre lo individual, comprometernos con la sociedad y trabajar en pos de la igualdad. Ser un club, una escuela y una familia. <br />
+            <b>SER DE RIVER ES VIVIR Y JUGAR CON GRANDEZA</b>
           </p>
-          <h2>Visión</h2>
+          <h2>NUESTROS OBJETIVOS</h2>
           <p>
-            La Escuela de Fútbol de River Plate busca ser un referente en la
-            formación de jugadores y personas, a través de un proceso de
-            enseñanza integral, que les permita a los alumnos alcanzar su máximo
-            potencial en el fútbol y en la vida.
+            <img src={escudo} alt=""  className="escudoObjetivos"/> {""}
+            Ser una institucion ejemplar por su excelentecia deportiva y su compromiso con el juego limpio, la educación, la cultura y la acción social.
+          </p>
+          <p>
+          <img src={escudo} alt=""  className="escudoObjetivos"/> {""}
+          Son los empleados, quienes forman parte de la vida diaria de River Plate, los que hacen que el club sea una institucion modelo
+          </p>
+          <p>
+          <img src={escudo} alt=""  className="escudoObjetivos"/> {""}
+          River se compromete con la excelencia deportiva y con el progreso social, elevando el nivel de la libertad de todos sus miembros
+          </p>
+          <p>
+          <img src={escudo} alt=""  className="escudoObjetivos"/> {""}
+          <b><i> Ser de River es vivir y jugar con grandeza. </i></b> 
           </p>
         </Col>
       </Row>

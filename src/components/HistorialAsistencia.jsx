26817a6effb5140ccb1dd6fa36e70f9a5f81fceb
Diego Olmi <diegolmi@hotmail.com>
2024-07-26 02:50:53 -0300

    fecha de asistencia y filtro

--- src/components/HistorialAsistencia.jsx
+++ src/components/HistorialAsistencia.jsx
@@ -1,23 +1,30 @@
-import { useContext, useState, useEffect } from "react";
-import { Container, Row, Col, Table, Form, Pagination, Alert } from "react-bootstrap";
-import { AlumnoContext } from "../context/AlumnoContext";
+import { useState, useEffect } from "react";
+import { Container, Row, Col, Table, Form } from "react-bootstrap";
 import axios from "axios";
-import Swal from "sweetalert2";
+import moment from "moment-timezone";
 
 const HistorialAsistencia = () => {
-  const { alumnosEscuela, getAlumnos } = useContext(AlumnoContext);
   const [asistencias, setAsistencias] = useState([]);
   const [fechaFiltro, setFechaFiltro] = useState("");
-  const [categoriaFiltro, setCategoriaFiltro] = useState("");
-  const [generoFiltro, setGeneroFiltro] = useState("");
-  const [nombreFiltro, setNombreFiltro] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const [loading, setLoading] = useState(false);
-  const itemsPerPage = 10;
 
-  useEffect(() => {
-    getAlumnos();
-  }, []);
+  const fetchAsistencias = async () => {
+    console.log(fechaFiltro)
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/asistencias?fecha=${fechaFiltro}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response.data, "Asistencias historial:");
+      setAsistencias(response.data);
+    } catch (error) {
+      console.error("Error al obtener asistencias:", error);
+    }
+  };
 
   useEffect(() => {
     if (fechaFiltro) {
@@ -25,54 +32,10 @@ const HistorialAsistencia = () => {
     } else {
       setAsistencias([]);
     }
-  }, [fechaFiltro, categoriaFiltro, generoFiltro, nombreFiltro]);
+  }, [fechaFiltro]);
 
-  const fetchAsistencias = async () => {
-    const token = localStorage.getItem("token");
-    setLoading(true);
-    try {
-      const response = await axios.get("https://backescuelariver.onrender.com/api/asistencias", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        params: {
-          fecha: fechaFiltro,
-          categoria: categoriaFiltro,
-          genero: generoFiltro,
-        },
-      });
-      setAsistencias(response.data);
-      setCurrentPage(1); // Reset pagination to the first page
-    } catch (error) {
-      Swal.fire("Error", "Hubo un problema al obtener las asistencias", "error");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const alumnosFiltrados = asistencias.filter(asistencia => {
-    const alumno = asistencia.alumnoId;
-    const asistenciaFecha = new Date(asistencia.fecha);
-    const filtroFecha = new Date(fechaFiltro);
-    const mismaFecha = asistenciaFecha.toDateString() === filtroFecha.toDateString();
-
-    return (
-      (!fechaFiltro || mismaFecha) &&
-      (categoriaFiltro === "" || alumno.categoria === parseInt(categoriaFiltro)) &&
-      (generoFiltro === "" || alumno.genero === generoFiltro) &&
-      (nombreFiltro === "" || alumno.nombre.toLowerCase().includes(nombreFiltro.toLowerCase()))
-    );
-  });
-
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = alumnosFiltrados.slice(indexOfFirstItem, indexOfLastItem);
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    return date.toLocaleDateString("es-ES");
+  const handleFechaChange = (e) => {
+    setFechaFiltro(e.target.value);
   };
 
   return (
@@ -81,96 +44,36 @@ const HistorialAsistencia = () => {
         <Col>
           <h1>Historial de Asistencia</h1>
           <Form.Group>
-            <Form.Label>Buscar por fecha</Form.Label>
+            <Form.Label>Fecha</Form.Label>
             <Form.Control
               type="date"
               value={fechaFiltro}
-              onChange={(e) => setFechaFiltro(e.target.value)}
+              onChange={handleFechaChange}
             />
           </Form.Group>
-          <Form.Group>
-            <Form.Label>Buscar por categoría</Form.Label>
-            <Form.Control
-              as="select"
-              value={categoriaFiltro}
-              onChange={(e) => setCategoriaFiltro(e.target.value)}
-            >
-              <option value="">Todas</option>
-              {[...new Set(alumnosEscuela.map((alumno) => alumno.categoria))].map(
-                (categoria) => (
-                  <option key={categoria} value={categoria}>
-                    {categoria}
-                  </option>
-                )
-              )}
-            </Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Buscar por género</Form.Label>
-            <Form.Control
-              as="select"
-              value={generoFiltro}
-              onChange={(e) => setGeneroFiltro(e.target.value)}
-            >
-              <option value="">Todos</option>
-              <option value="Masculino">Masculino</option>
-              <option value="Femenino">Femenino</option>
-            </Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Buscar por nombre</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Buscar por nombre"
-              value={nombreFiltro}
-              onChange={(e) => setNombreFiltro(e.target.value)}
-            />
-          </Form.Group>
-          {loading ? (
-            <Alert variant="info" className="mt-3">
-              Cargando...
-            </Alert>
-          ) : alumnosFiltrados.length > 0 ? (
-            <>
-              <Table striped bordered hover className="mt-3">
-                <thead>
-                  <tr>
-                    <th>Nombre</th>
-                    <th>Apellido</th>
-                    <th>Categoría</th>
-                    <th>Género</th>
-                    <th>Fecha de Asistencia</th>
+          {asistencias.length > 0 ? (
+            <Table striped bordered hover className="mt-3">
+              <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <th>Apellido</th>
+                  <th>Categoría</th>
+                  <th>Fecha de Asistencia</th>
+                </tr>
+              </thead>
+              <tbody>
+                {asistencias.map((asistencia) => (
+                  <tr key={asistencia._id}>
+                    <td>{asistencia.alumnoId.nombre}</td>
+                    <td>{asistencia.alumnoId.apellido}</td>
+                    <td>{asistencia.alumnoId.categoria}</td>
+                    <td>{moment(asistencia.fecha).tz("America/Argentina/Buenos_Aires").format('DD-MM-YYYY')}</td>
                   </tr>
-                </thead>
-                <tbody>
-                  {currentItems.map((asistencia) => {
-                    const alumno = asistencia.alumnoId;
-                    return (
-                      <tr key={asistencia._id}>
-                        <td>{alumno.nombre}</td>
-                        <td>{alumno.apellido}</td>
-                        <td>{alumno.categoria}</td>
-                        <td>{alumno.genero}</td>
-                        <td>{formatDate(asistencia.fecha)}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </Table>
-              <Pagination>
-                {Array.from({ length: Math.ceil(alumnosFiltrados.length / itemsPerPage) }, (_, index) => (
-                  <Pagination.Item key={index + 1} active={index + 1 === currentPage} onClick={() => paginate(index + 1)}>
-                    {index + 1}
-                  </Pagination.Item>
                 ))}
-              </Pagination>
-            </>
+              </tbody>
+            </Table>
           ) : (
-            !loading && fechaFiltro && (
-              <Alert variant="info" className="mt-3">
-                Fecha sin actividad
-              </Alert>
-            )
+            <p>No hay registros de asistencia para esta fecha.</p>
           )}
         </Col>
       </Row>
--- src/components/RegistroAsistencia.jsx
+++ src/components/RegistroAsistencia.jsx
@@ -1,16 +1,16 @@
 import { useContext, useState, useEffect } from "react";
-import { Container, Row, Col, Table, Form, Button, Alert, Pagination } from "react-bootstrap";
+import { Container, Row, Col, Table, Button, Form } from "react-bootstrap";
 import { AlumnoContext } from "../context/AlumnoContext";
 import axios from "axios";
 import Swal from "sweetalert2";
+import moment from "moment";
 
 const RegistroAsistencia = () => {
   const { alumnosEscuela, getAlumnos } = useContext(AlumnoContext);
   const [alumnosFiltrados, setAlumnosFiltrados] = useState([]);
-  const [nombreFiltro, setNombreFiltro] = useState("");
   const [fechaFiltro, setFechaFiltro] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
+  const [asistencias, setAsistencias] = useState([]);
+  const [nombreFiltro, setNombreFiltro] = useState("");
 
   useEffect(() => {
     getAlumnos();
@@ -20,87 +20,82 @@ const RegistroAsistencia = () => {
     if (fechaFiltro) {
       fetchAsistencias();
     } else {
-      setAlumnosFiltrados(alumnosEscuela);
+      setAlumnosFiltrados([]);
     }
-  }, [fechaFiltro, alumnosEscuela]);
-
-  useEffect(() => {
-    if (!fechaFiltro) {
-      setAlumnosFiltrados(alumnosEscuela.filter(alumno =>
-        alumno.nombre.toLowerCase().includes(nombreFiltro.toLowerCase())
-      ));
-    }
-  }, [nombreFiltro, alumnosEscuela, fechaFiltro]);
+  }, [fechaFiltro]);
 
   const fetchAsistencias = async () => {
     const token = localStorage.getItem("token");
     try {
-      const response = await axios.get("https://backescuelariver.onrender.com/api/asistencias", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        params: { fecha: fechaFiltro },
-      });
-      const asistenciasRegistradas = response.data;
-      const alumnosConAsistencia = asistenciasRegistradas.map(asistencia => asistencia.alumnoId._id);
-      const alumnosDisponibles = alumnosEscuela.filter(alumno => !alumnosConAsistencia.includes(alumno._id));
-      setAlumnosFiltrados(alumnosDisponibles);
-    } catch (error) {
-      Swal.fire("Error", "Hubo un problema al obtener las asistencias", "error");
-    }
-  };
-
-  const handleAsistencia = async (alumnoId) => {
-    const token = localStorage.getItem("token");
-    try {
-      await axios.post(
-        "https://backescuelariver.onrender.com/api/asistencias",
-        { alumnoId, fecha: fechaFiltro },
+      const response = await axios.get(
+        `http://localhost:8080/api/asistencias?fecha=${fechaFiltro}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         }
       );
+      console.log("Asistencias obtenidas del servidor:", response.data);
+      setAsistencias(response.data);
+    } catch (error) {
+      console.error("Error al obtener asistencias:", error);
+    }
+  };
+
+  useEffect(() => {
+    const alumnosConAsistencia = asistencias.map((a) => a.alumnoId._id);
+    console.log("IDs de alumnos con asistencia registrada en la fecha:", alumnosConAsistencia);
+    setAlumnosFiltrados(
+      alumnosEscuela.filter(
+        (alumno) =>
+          alumno.nombre.toLowerCase().includes(nombreFiltro.toLowerCase()) &&
+          !alumnosConAsistencia.includes(alumno._id)
+      )
+    );
+  }, [nombreFiltro, alumnosEscuela, asistencias]);
+
+  const handleAsistencia = async (alumnoId) => {
+    const token = localStorage.getItem("token");
+    const fechaFormateada = moment(fechaFiltro).format("DD-MM-YYYY");
+
+    console.log("Fecha enviada:", fechaFormateada);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/asistencias",
+        { alumnoId, fecha: fechaFormateada },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("Respuesta del servidor al registrar asistencia:", response.data);
       Swal.fire(
         "Asistencia Registrada",
         "La asistencia ha sido registrada correctamente",
         "success"
       );
-      fetchAsistencias(); // Actualiza la lista de alumnos disponibles
+      fetchAsistencias();
     } catch (error) {
+      console.error("Error al registrar asistencia:", error);
       if (error.response && error.response.status === 401) {
         Swal.fire("Error", "Acceso denegado, token no válido", "error");
       } else {
-        Swal.fire("Error", error.response.data.message || "Hubo un problema al registrar la asistencia", "error");
+        Swal.fire(
+          "Error",
+          error.response.data.message || "Hubo un problema al registrar la asistencia",
+          "error"
+        );
       }
     }
   };
-
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = alumnosFiltrados
-    .filter(alumno => alumno.nombre.toLowerCase().includes(nombreFiltro.toLowerCase()))
-    .slice(indexOfFirstItem, indexOfLastItem);
-  const totalPages = Math.ceil(alumnosFiltrados.length / itemsPerPage);
 
   return (
     <Container>
       <Row className="mt-5">
         <Col>
           <h1>Registro de Asistencia</h1>
-          <Form.Group>
-            <Form.Label>Buscar por fecha</Form.Label>
-            <Form.Control
-              type="date"
-              value={fechaFiltro}
-              onChange={(e) => setFechaFiltro(e.target.value)}
-            />
-          </Form.Group>
           <Form.Group>
             <Form.Label>Buscar por nombre</Form.Label>
             <Form.Control
@@ -110,51 +105,44 @@ const RegistroAsistencia = () => {
               onChange={(e) => setNombreFiltro(e.target.value)}
             />
           </Form.Group>
-          {currentItems.length > 0 ? (
-            <>
-              <Table striped bordered hover className="mt-3">
-                <thead>
-                  <tr>
-                    <th>Nombre</th>
-                    <th>Apellido</th>
-                    <th>Categoría</th>
-                    <th>Acciones</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {currentItems.map((alumno) => (
-                    <tr key={alumno._id}>
-                      <td>{alumno.nombre}</td>
-                      <td>{alumno.apellido}</td>
-                      <td>{alumno.categoria}</td>
-                      <td>
-                        <Button
-                          variant="success"
-                          onClick={() => handleAsistencia(alumno._id)}
-                        >
-                          Registrar Asistencia
-                        </Button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-              <Pagination className="mt-3">
-                {Array.from({ length: totalPages }, (_, index) => (
-                  <Pagination.Item
-                    key={index + 1}
-                    active={index + 1 === currentPage}
-                    onClick={() => handlePageChange(index + 1)}
-                  >
-                    {index + 1}
-                  </Pagination.Item>
-                ))}
-              </Pagination>
-            </>
-          ) : (
-            <Alert variant="info" className="mt-3">
-              No hay alumnos disponibles para registrar asistencia en esta fecha.
-            </Alert>
+          <Form.Group>
+            <Form.Label>Fecha</Form.Label>
+            <Form.Control
+              type="date"
+              value={fechaFiltro}
+              onChange={(e) => setFechaFiltro(e.target.value)}
+              max={moment().format("YYYY-MM-DD")} // Deshabilita fechas futuras
+            />
+          </Form.Group>
+          <Table striped bordered hover className="mt-3">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Categoría</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {alumnosFiltrados.map((alumno) => (
+                <tr key={alumno._id}>
+                  <td>{alumno.nombre}</td>
+                  <td>{alumno.apellido}</td>
+                  <td>{alumno.categoria}</td>
+                  <td>
+                    <Button
+                      variant="success"
+                      onClick={() => handleAsistencia(alumno._id)}
+                    >
+                      Registrar Asistencia
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          {alumnosFiltrados.length === 0 && (
+            <p>No hay alumnos disponibles para registrar asistencia en esta fecha.</p>
           )}
         </Col>
       </Row>
